0d28a71e04332db0ad5c2392645a479c6733404b
Guilherme Rodrigues de Melo <melo.guilhermer@gmail.com>
2021-08-25 02:18:55 -0300

    Resolvido

--- grains/src/grains.clj
+++ grains/src/grains.clj
@@ -1,19 +1,8 @@
 (ns grains)
 
 (defn square [square-number]
-  (cond
-    (= square-number 1) 1
-    (= square-number 2) 2
-    :else (loop [actual-square 3
-                 previous-result 2]
-            (if (= actual-square square-number)
-              (*' previous-result 2)
-              (recur (inc actual-square) (* previous-result 2))))))
+  (->> (repeat (dec square-number) 2)
+       (reduce *')))
 
 (defn total []
-  (loop [actual-square 1
-         previous-result 1
-         result 0]
-    (if (= actual-square 64)
-      result
-      (recur (inc actual-square) (*' previous-result 2) (+' previous-result (*' previous-result 2))))))
+  (reduce +' (map square (range 1 65))))
